48b50e6aff2bf54973fe3a8032f3124687ee1ad1
siphokazi <usphow@gmail.com>
2023-09-05 10:32:04 -0700

    islower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def islower(c):
+
+    return ord('a') <= ord(c) <= ord('z')
+
+print(islower('a'))
+print(islower('H'))
+print(islower('A'))
+print(islower('3'))
+print(islower('g'))
