77034acad5422542dd42836f0d87e009c5da67fc
siphokazi <usphow@gmail.com>
2023-09-05 10:41:13 -0700

    uppercae

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def uppercase(s):
+    result = ""
     for char in s:
         if 'a' <= char <= 'z':
             upper_char = chr(ord(char) - ord('a') + ord('A'))
         else:
             upper_char = char
-        print(upper_char, end='')
-    print()
+        result += upper_char
+    print("{}".format(result))
 
 
 uppercase("Best School 98 Battery Street")
