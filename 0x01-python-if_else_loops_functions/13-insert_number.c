7c425b11f0e6aedd1dd97f0a9790d352683623ed
siphokazi <usphow@gmail.com>
2023-09-06 03:30:51 -0700

    i got wockhardt in my system

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,6 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * insert_node - Inserts a number into a sorted singly linked list.
@@ -11,7 +9,7 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node, *current;
+	listint_t *new_node, *current, *prev;
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
@@ -20,78 +18,26 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->n = number;
 	new_node->next = NULL;
 
-	if (!*head || number < (*head)->n)
+	current = *head;
+	prev = NULL;
+
+	while (current && current->n < number)
+	{
+		prev = current;
+		current = current->next;
+	}
+
+	if (!prev)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return (new_node);
 	}
 
-	current = *head;
-	while (current->next && current->next->n < number)
-		current = current->next;
-
-	new_node->next = current->next;
-	current->next = new_node;
+	else
+	{
+		prev->next = new_node;
+		new_node->next = current;
+	}
 
 	return (new_node);
-}
-
-/**
- * print_listint - Prints a linked list of integers.
- * @h: Pointer to the head of the linked list.
- */
-void print_listint(const listint_t *h)
-{
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-}
-
-/**
- * free_listint - Frees a linked list.
- * @head: Pointer to the head of the linked list.
- */
-void free_listint(listint_t *head)
-{
-	listint_t *tmp;
-
-	while (head)
-	{
-		tmp = head;
-		head = head->next;
-		free(tmp);
-	}
-}
-
-/**
- * main - check the code for
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	listint_t *head = NULL;
-
-	add_nodeint_end(&head, 0);
-	add_nodeint_end(&head, 1);
-	add_nodeint_end(&head, 2);
-	add_nodeint_end(&head, 3);
-	add_nodeint_end(&head, 4);
-	add_nodeint_end(&head, 98);
-	add_nodeint_end(&head, 402);
-	add_nodeint_end(&head, 1024);
-	print_listint(head);
-
-	printf("-----------------\n");
-
-	insert_node(&head, 27);
-
-	print_listint(head);
-
-	free_listint(head);
-
-	return (0);
 }
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,16 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ *
  */
 typedef struct listint_s
 {
@@ -18,9 +15,10 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
-listint_t *insert_node(listint_t **head, int number);
-void print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+
+listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
