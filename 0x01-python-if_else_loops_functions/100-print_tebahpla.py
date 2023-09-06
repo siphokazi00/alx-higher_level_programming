3f4651c7be04329d8aa14fdddd114b76840f3dda
siphokazi <usphow@gmail.com>
2023-09-06 10:11:59 -0700

    ByteCode -> Python #2

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,6 +3,6 @@ for char_code in range(ord('z'), ord('a') - 1, -1):
     char = chr(char_code)
     if char_code % 2 == 0:
         char = char.upper()
-    print(char, end='')
+    print("{char, end=''}".format(char))
 
 print()
