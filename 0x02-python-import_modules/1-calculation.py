9dc5b13b8b99c6f820141ff54af00cba6c206f85
siphokazi <usphow@gmail.com>
2023-09-10 08:04:52 -0700

    calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+result_add = add(a, b)
+print("{} + {} = {}".format(a, b, result_add))
+
+result_sub = sub(a, b)
+print("{} - {} = {}".format(a, b, result_sub))
+
+result_mul = mul(a, b)
+print("{} * {} = {}".format(a, b, result_mul))
+
+result_div = div(a, b)
+print("{} / {} = {}".format(a, b, result_div))
