ebd8350e5739755d0e0b1eea145f575b7fcec698
siphokazi <usphow@gmail.com>
2023-09-16 15:09:46 +0000

    haibo

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
 	new_matrix = []
 
 	for rows in matrix:
-		new_row[]
+		new_row = []
 		for element in row:
 			new_row.append(element ** 2)
 		new_matrix.append(new_row)
