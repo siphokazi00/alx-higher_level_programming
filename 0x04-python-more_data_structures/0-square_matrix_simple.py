c7fbef8527d2fa414831d1c467b26bc2d412cf74
siphokazi <usphow@gmail.com>
2023-09-16 15:06:51 +0000

    haibo

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+	new_matrix = []
+
+	for rows in matrix:
+		new_row[]
+		for element in row:
+			new_row.append(element ** 2)
+		new_matrix.append(new_row)
+
+	return new_matrix
+
+if __name__ == "__main__":
+	matrix = [
+		[1, 2, 3],
+		[4, 5, 6],
+		[7, 7, 9]
+	]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
