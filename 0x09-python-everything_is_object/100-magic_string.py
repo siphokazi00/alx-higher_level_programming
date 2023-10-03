b695085b78352410da44129a20193bf5879dd4fa
siphokazi <usphow@gmail.com>
2023-10-03 03:40:16 -0700

    #pythonic

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,6 @@
+def magic_string():
+    if not hasattr(magic_string, "count"):
+        magic_string.count = 1
+    else:
+        magic_string.count += 1
+    return "BestSchool" + ", BestSchool" * (magic_string.count - 1)
