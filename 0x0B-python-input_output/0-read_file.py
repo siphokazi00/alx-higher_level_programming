28f1f8ddfb4a600825eaba758288fb4332866c30
siphokazi <usphow@gmail.com>
2023-10-10 06:59:33 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a function that reads a text file and prints it to stdout."""
+
+
 def read_file(filename=""):
     """Reads a text file UTF8 and prints it to stdout."""
 
