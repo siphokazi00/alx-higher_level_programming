53b5a548966f821d58f9dcd201bbdfe5dce65655
siphokazi <usphow@gmail.com>
2023-10-11 05:04:04 -0700

    Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a function that returns a list of lists of ints"""
+
+
+def print_triangle(triangle):
+    """Represents Pascal's Triangle of size n."""
+
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
