462b88272050218b723fcbdf382e70d23eaa823f
siphokazi <usphow@gmail.com>
2023-11-14 13:09:27 -0800

    Lists all tables

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,3 @@
 -- This script creates a table first_table
-CREATE TABLE IF NOT EXISTS `first_table` (
-	`id` INT,
-	`name` VARCHAR(256),
-);
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
 
