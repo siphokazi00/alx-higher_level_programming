c2d38d45569fdb2d20eb6bb72ccd171483578ae7
siphokazi <usphow@gmail.com>
2023-11-15 04:27:09 -0800

    say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- This script lists all records of second_table
-SELECT * FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC;
+SELECT `score`, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC;
 
