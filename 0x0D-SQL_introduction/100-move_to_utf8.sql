c644fef0a43518e76aaa491bc5506a1251320b09
siphokazi <usphow@gmail.com>
2023-11-15 10:15:42 -0800

    go to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- This script converts a database hbtn_0c_0 to UTF8
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE `first_table` MODIFY `name` VARCHAR CHARACTER SET utf8 COLLATE utf8_general_ci;
