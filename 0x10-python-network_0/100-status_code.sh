552cbedb5697dc879236f04d1ea1b62ddef739c5
siphokazi <usphow@gmail.com>
2024-03-27 12:06:42 -0700

    Only status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends req to URL passed as arg, display response status code only
+# Sends req to URL passed as arg, displays response status code
 url="$1"; curl -s -o /dev/null -w "%{http_code}" "$url"
