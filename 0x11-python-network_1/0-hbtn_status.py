fe6a1fbb6f1de6dda0f7acc442eb2b02948dc3f0
siphokazi <usphow@gmail.com>
2024-03-27 15:25:30 +0000

    What's my status #0?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        content = response.read().decode("utf-8")
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
